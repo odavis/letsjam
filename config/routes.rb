b86fd54aafc8003261eb183dda90cf1468eca2c1
Chris Scott <Chris@Gnmlbs.com>
2014-03-22 16:05:14 -0400

    Frontend changes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Letsjam::Application.routes.draw do
   get "static/about"
 
   root 'static#home'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
