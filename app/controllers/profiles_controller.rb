dcf548246cb1cc37ed5d860fd2f73cc72c7318c7
Orrett Davis <orrett.davis@gmail.com>
2014-03-22 14:04:33 -0400

    updates to user Profile controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -10,17 +10,28 @@ class ProfilesController < ApplicationController
   end
 
   def show
-  end
-
-  def new
-  end
-
-  def create
+    @user = User.find(params[:id])
+    @profile = @user.profile
   end
 
   def update
+    @user = current_user
+    @profile = @user.profile
+
+    if @profile.update_for_profile(profile_params)
+      flash[:notice] = "Profile Update"
+      redirect_to profile_path(current_user)
+    else
+      render :edit
+    end
   end
 
   def destroy
   end
+
+  private
+  def profile_params
+    params.require(:profile).permit(:bio, :name)
+  end
+
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,9 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
+
+def update_for_profile(profile_params)
+  self.attributes = profile_params
+  self.save
+end
+
 end
